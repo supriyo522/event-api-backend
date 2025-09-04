e0762b049c44523bee6aa3bf35ee4fc280fb0863
supriyo522 <134998523+supriyo522@users.noreply.github.com>
2025-09-04 15:15:24 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
+
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as express from 'express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
   app.useGlobalPipes(new ValidationPipe());
 
+  app.use('/uploads', express.static(join(__dirname, '..', process.env.UPLOADS_FOLDER || 'uploads')));
 
-  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
-
-  await app.listen(3000);
-  console.log('Server running on http://localhost:3000');
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Server running on http://localhost:${port}`);
 }
 bootstrap();
